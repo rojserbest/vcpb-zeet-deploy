e0141ab876e401572ef412c6b6f56b0e5dfeb569
Guru <76265102+Guru-2509@users.noreply.github.com>
2021-02-05 14:48:57 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ $UID -eq 0 ]; then
-user=$1vcpb #Make change here if you changed username
+user=$1fahsg718u #Make change here if you changed username
 exec su "$user" "$0" -- "$@"
 fi
 echo "This will be run from user $UID"
 cp -f /home/__init__.py /home/vcpb/config/__init__.py
-mv /home/Telegram /home/vcpb/Telegram #Move Telegram to VCPB's Home (make changes according to username)
+mv /home/Telegram /home/fahsg718u/Telegram #Move Telegram to VCPB's Home (make changes according to username)
 pactl load-module module-null-sink sink_name=MySink #loading MySink
 echo "Pulseaudio MySink Loaded"
 pactl set-default-sink MySink #setting Mysink as default Mic
